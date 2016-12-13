7bb718fc77cfceca0b3b6eef04c0022fbe37c6ab
Nikola Serdiuk <nikola.serdiuk@gmail.com>
2016-12-13 18:57:52 +0200

    CreditCardInput Component finish

--- exercises/CreditCardInput.jsx
+++ exercises/CreditCardInput.jsx
@@ -4,11 +4,40 @@ class CreditCardInput extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {number: ''}
+    this.state = {number: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.checkType = this.checkType.bind(this);
   }
 
   process(number) {
-    return { text: '', type: ''}
+    let type = this.checkType(number);
+    let text = this.insertSpaces(number);
+    text = text.trim();
+    return { text: text, type: type}
+  }
+
+  insertSpaces(text) {
+    return text.replace(/(.{4})/g, '$1 ')
+  }
+
+  checkType(text) {
+    let types = this.props.types;
+    for(var type in types) {
+      if(text.match(types[type])) {
+        return type;
+      }
+    }
+    return '';
+  }
+
+  handleChange(e) {
+    let newValue = e.target.value;
+    let newNumber = this.filterWhitespace(newValue);
+    this.setState({number: newNumber});
+  }
+
+  filterWhitespace(text) {
+    return text.replace(/\s/g, '');
   }
 
   render() {
@@ -16,7 +45,7 @@ class CreditCardInput extends React.Component {
 
     return (
       <div>
-        <input type="text" value={text}/>
+        <input type="text" onChange={this.handleChange} value={text}/>
         <input type="text" value={type}/>
       </div>
     )
